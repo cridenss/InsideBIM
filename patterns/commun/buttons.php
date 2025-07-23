13afa43e27f57084d6d08575e0573513a83a1873
cridenss <bapt.reve@gmail.com>
2025-07-23 18:03:21 +0200

    avant modif hero/header

--- patterns/commun/buttons.php
+++ patterns/commun/buttons.php
@@ -1,5 +1,5 @@
 <!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-buttons" onclick="toggleCard()"><!-- wp:button -->
     <div class="wp-block-button"><a class="wp-block-button__link wp-element-button"></a></div>
     <!-- /wp:button -->
 </div>
